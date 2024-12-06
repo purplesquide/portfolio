7ae4b89127273052d860c285dc83874e98048b4c
Ouassim Abdeljalil Touati <ouassim.touati@univ-constantine2.dz>
2024-12-06 18:27:43 +0100

    imgchange

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -17,6 +17,17 @@ const Hero = () => {
           <p className="text-gray-400 py-4 max-w-[700px]">
             I specialize in building exceptional digital experiences.
           </p>
+
+          <div className="md:w-1/2 mt-8 md:mt-0">
+          <div className="relative w-64 h-64 md:w-[300px] md:h-[300px] mx-auto">
+            <div className="absolute inset-0 bg-[#F3FFB6] rounded-full blur-3xl opacity-20"></div>
+            <img
+              src={profileImage}
+              alt="Professional headshot"
+              className="relative z-10 w-full h-full rounded-full object-cover border-4 border-[#F3FFB6]"
+            />
+          </div>
+        </div>
           
           {/* CTA Buttons */}
           <div className="flex flex-col sm:flex-row gap-4 mt-4">
@@ -31,17 +42,8 @@ const Hero = () => {
           </div>
         </div>
 
-        {/* Image */}
-        <div className="md:w-1/2 mt-8 md:mt-0">
-          <div className="relative w-64 h-64 md:w-[300px] md:h-[300px] mx-auto">
-            <div className="absolute inset-0 bg-[#F3FFB6] rounded-full blur-3xl opacity-20"></div>
-            <img
-              src={profileImage}
-              alt="Professional headshot"
-              className="relative z-10 w-full h-full rounded-full object-cover border-4 border-[#F3FFB6]"
-            />
-          </div>
-        </div>
+        
+        
       </div>
     </div>
   );
